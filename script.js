85996f12177cc62f12f98444465ed8d88e8bc641
serrathvs <monserrathvs8qgmail.com>
2024-08-11 00:32:45 -0600

    primer

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('Todos los campos son obligatorios.');
+        event.preventDefault(); 
+    }
+});
